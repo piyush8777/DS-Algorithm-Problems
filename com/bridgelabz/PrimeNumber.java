b6aeddff048123c750b619e9673fbd603b5c3329
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-17 00:43:27 +0530

    Added :Prime number in 0-1000 range

---
+++ com/bridgelabz/PrimeNumber.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import com.bridgelabz.utility.Utility;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		Utility.printPrimes();
+		System.out.println("==============");
+		Utility.printPrimePalindrome();
+		System.out.println("==============");
+		Utility.primeAnagram();
+	}
+
+
+}
